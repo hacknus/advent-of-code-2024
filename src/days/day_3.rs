8e346e0476e1374e18e10d401741fdd6423346d8
hacknus <linus.stoeckli@unibe.ch>
2024-12-03 09:08:18 +0100

    cleanup

--- src/days/day_3.rs
+++ src/days/day_3.rs
@@ -1,14 +1,7 @@
-use crate::io::read_file_lines;
 use crate::problem::Problem;
 use regex::Regex;
 use std::fs;
 use std::path::Path;
-use std::str::FromStr;
-
-fn atoi<F: FromStr>(input: &str) -> Result<F, <F as FromStr>::Err> {
-    let i = input.find(|c: char| !c.is_numeric()).unwrap_or(input.len());
-    input[..i].parse::<F>()
-}
 pub struct DayThree {}
 
 impl Problem for DayThree {
