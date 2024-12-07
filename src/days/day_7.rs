5cd3c861c2fb437534c348527f413dd2993818ce
hacknus <linus.stoeckli@unibe.ch>
2024-12-07 22:58:22 +0100

    rayonrayonrayon

--- src/days/day_7.rs
+++ src/days/day_7.rs
@@ -56,42 +56,44 @@ impl Problem for DaySeven {
 
     fn part_two(&self, input: &Path) -> String {
         let content = read_file_lines(input);
-        let mut sum = 0;
         let operators = ["+", "*", "|"];
-        'line_loop: for line in content.iter() {
-            let splits = line.split(":").collect::<Vec<&str>>();
-            let result = splits[0].parse::<i64>().unwrap();
-            let parts = splits[1]
-                .split_whitespace()
-                .map(|p| p.parse::<i64>().unwrap())
-                .collect::<Vec<i64>>();
+        let sum: i64 = content
+            .into_par_iter()
+            .map(|line| {
+                let splits = line.split(":").collect::<Vec<&str>>();
+                let result = splits[0].parse::<i64>().unwrap();
+                let parts = splits[1]
+                    .split_whitespace()
+                    .map(|p| p.parse::<i64>().unwrap())
+                    .collect::<Vec<i64>>();
 
-            let n = parts.len();
-            let combinations = (1..n).map(|_| operators).multi_cartesian_product();
-            'combination_loop: for combination in combinations {
-                let mut parts_to_calculate = parts.clone();
-                for i in 0..n - 1 {
-                    match combination[i] {
-                        "*" => parts_to_calculate[i + 1] *= parts_to_calculate[i],
-                        "+" => parts_to_calculate[i + 1] += parts_to_calculate[i],
-                        "|" => {
-                            parts_to_calculate[i + 1] =
-                                concat(parts_to_calculate[i], parts_to_calculate[i + 1])
+                let n = parts.len();
+                let combinations = (1..n).map(|_| operators).multi_cartesian_product();
+                'combination_loop: for combination in combinations {
+                    let mut parts_to_calculate = parts.clone();
+                    for i in 0..n - 1 {
+                        match combination[i] {
+                            "*" => parts_to_calculate[i + 1] *= parts_to_calculate[i],
+                            "+" => parts_to_calculate[i + 1] += parts_to_calculate[i],
+                            "|" => {
+                                parts_to_calculate[i + 1] =
+                                    concat(parts_to_calculate[i], parts_to_calculate[i + 1])
+                            }
+                            _ => {
+                                unreachable!()
+                            }
                         }
-                        _ => {
-                            unreachable!()
+                        if parts_to_calculate[i + 1] > result {
+                            continue 'combination_loop;
                         }
                     }
-                    if parts_to_calculate[i + 1] > result {
-                        continue 'combination_loop;
+                    if parts_to_calculate.last().unwrap() == &result {
+                        return result;
                     }
                 }
-                if parts_to_calculate.last().unwrap() == &result {
-                    sum += result;
-                    continue 'line_loop;
-                }
-            }
-        }
+                0
+            })
+            .sum();
         format!("{}", sum)
     }
 }
