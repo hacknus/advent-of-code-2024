43ca5b7dd07721aeaa335c95ead8df2db7785184
hacknus <linus.stoeckli@unibe.ch>
2024-12-11 08:54:37 +0100

    recursive, still too slow

--- src/days/day_11.rs
+++ src/days/day_11.rs
@@ -1,71 +1,69 @@
-use crate::io::read_file_lines;
 use crate::problem::Problem;
-use std::collections::HashMap;
+use std::cell::RefCell;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::path::Path;
+use std::rc::Rc;
 
 pub struct DayEleven {}
+
+pub fn branch(blink: usize, max_blink: usize, number: usize) -> usize {
+    if blink >= max_blink {
+        return 1;
+    }
+    let mut sum = 0;
+    if number == 0 {
+        sum += branch(blink + 1, max_blink, 1);
+    } else if number.to_string().len() % 2 == 0 {
+        let left = number
+            .to_string()
+            .chars()
+            .take(number.to_string().len() / 2)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        let right = number
+            .to_string()
+            .chars()
+            .skip(number.to_string().len() / 2)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        sum += branch(blink + 1, max_blink, left);
+        sum += branch(blink + 1, max_blink, right);
+    } else {
+        sum += branch(blink + 1, max_blink, number * 2024);
+    }
+    sum
+}
 
 impl Problem for DayEleven {
     fn part_one(&self, input: &Path) -> String {
         let mut stones = fs::read_to_string(input)
             .unwrap()
             .split_whitespace()
-            .map(|s| s.to_string())
-            .collect::<Vec<String>>();
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
 
-        println!("{:?}", stones);
-
-        let mut states: HashMap<String, Vec<String>> = HashMap::new();
-        for blink in 0..25 {
-            dbg!(blink);
-            let mut i = 0;
-            loop {
-                if let Some(state) = states.get(&stones[i]) {
-                    stones[i] = state[0].clone();
-                    if state.len() == 2 {
-                        stones.insert(i + 1, state[1].clone());
-                        i += 1;
-                    }
-                } else if stones[i] == "0" {
-                    states.insert(stones[i].clone(), vec!["1".to_string()]);
-                    stones[i] = "1".to_string();
-                } else if stones[i].len() % 2 == 0 {
-                    let left = stones[i]
-                        .chars()
-                        .take(stones[i].len() / 2)
-                        .collect::<String>()
-                        .parse::<usize>()
-                        .unwrap();
-                    let right = stones[i]
-                        .chars()
-                        .skip(stones[i].len() / 2)
-                        .collect::<String>()
-                        .parse::<usize>()
-                        .unwrap();
-                    states.insert(stones[i].clone(), vec![left.to_string(), right.to_string()]);
-                    stones[i] = left.to_string();
-                    stones.insert(i + 1, right.to_string());
-                    i += 1;
-                } else {
-                    states.insert(
-                        stones[i].clone(),
-                        vec![format!("{}", stones[i].parse::<usize>().unwrap() * 2024)],
-                    );
-                    stones[i] = format!("{}", stones[i].parse::<usize>().unwrap() * 2024);
-                }
-
-                i += 1;
-                if i >= stones.len() {
-                    break;
-                }
-            }
+        let mut sum = 0;
+        for stone in stones {
+            sum += branch(0, 25, stone);
         }
 
-        format!("{}", stones.len())
+        format!("{}", sum)
     }
 
     fn part_two(&self, input: &Path) -> String {
+        let mut stones = fs::read_to_string(input)
+            .unwrap()
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+
+        let mut sum = 0;
+        for stone in stones {
+            sum += branch(0, 75, stone);
+        }
         format!("{}", "Part two not yet implemented.")
     }
 }
