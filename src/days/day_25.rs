212360459225968c4074f4f5589bf0d8909dceae
hacknus <linus.stoeckli@unibe.ch>
2024-12-25 09:47:35 +0100

    walk in the park

--- src/days/day_25.rs
+++ src/days/day_25.rs
@@ -1,11 +1,50 @@
 use crate::problem::Problem;
+use std::collections::HashSet;
+use std::fs;
 use std::path::Path;
 
 pub struct DayTwentyFive {}
 
 impl Problem for DayTwentyFive {
     fn part_one(&self, input: &Path) -> String {
-        format!("{}", "Part one not yet implemented.")
+        let content = fs::read_to_string(input).unwrap();
+        let mut keys = HashSet::new();
+        let mut locks = HashSet::new();
+        for entry in content.split("\n\n") {
+            let mut arr = [0; 5];
+            for line in entry.split("\n").skip(1).take(5) {
+                for (j, c) in line.chars().enumerate() {
+                    if c == '#' {
+                        arr[j] += 1;
+                    }
+                }
+            }
+            if entry.starts_with("#####") {
+                locks.insert(arr);
+            } else if entry.starts_with(".....") {
+                keys.insert(arr);
+            } else {
+                unreachable!()
+            }
+        }
+
+        let mut sum = 0;
+
+        for key in keys.iter() {
+            for lock in locks.iter() {
+                let mut overlap = false;
+                for (k, l) in key.iter().zip(lock.iter()) {
+                    if 5 - *k < *l {
+                        overlap = true;
+                    }
+                }
+                if !overlap {
+                    sum += 1;
+                }
+            }
+        }
+
+        format!("{}", sum)
     }
 
     fn part_two(&self, input: &Path) -> String {
